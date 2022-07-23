831c6cc4759dc71b5fb6b110c4f36235999390f8
germeeron <germeeron@gmail.com>
2022-07-23 10:43:00 +0800

    finished tictactoe

--- index2.js
+++ index2.js
@@ -21,17 +21,18 @@ let turnCounter = 0;
 let currentPlayer = "X";
 let running = false;
 
-initializeGame();
+startGame();
 
-function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+function startGame(){
+    cells.forEach(cell => cell.addEventListener("click", clickCell));
     previousButton.addEventListener("click", previousTurn);
+    nextButton.addEventListener("click", nextTurn);
     restartButton.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
 
-function cellClicked(){
+function clickCell(){
     const cellIndex = this.getAttribute("cellIndex");
 
     if(placeHolder[cellIndex] != "" || !running){
@@ -40,11 +41,14 @@ function cellClicked(){
 
     updateCell(this, cellIndex);
     checkWinner();
+    turnCounter++;
 }
 
 function updateCell(cell, index){
+
     placeHolder[index] = currentPlayer;
     cell.textContent = currentPlayer;
+    
     moveArray.push(index);
 }
 
@@ -73,10 +77,14 @@ function checkWinner(){
 
     if(roundWon){
         statusText.textContent = `${currentPlayer} wins!`;
+        previousButton.disabled = false;
+        nextButton.disabled = false;
         running = false;
     }
     else if(!placeHolder.includes("")){
         statusText.textContent = `Draw!`;
+        previousButton.disabled = false;
+        nextButton.disabled = false;
         running = false;
     }
     else{
@@ -89,15 +97,41 @@ function restartGame(){
     placeHolder = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
+    previousButton.disabled = true;
+    nextButton.disabled = true;
+    while (moveArray.length) moveArray.pop();
+    turnCounter = 0;
     running = true;
 }
+
 
 function previousTurn(){
     
     if(turnCounter === 0){
+        alert("oops!");
         return;
     }
-    //placeHolder.splice(moveArray[turnCounter - 1],1)//
-    let lastMove = document.getAttribute(`cellIndex${moveArray[turnCounter - 1]}`)//
-    placeHolder.splice(lastMove,1)
+
+    let lastMove = document.querySelector(`[cellIndex="${moveArray[turnCounter - 1]}"]`); //cell index of the last turn
+    placeHolder.splice(moveArray[turnCounter - 1],1);
+    
+    lastMove.textContent = "";
+    turnCounter--;
+}
+
+function nextTurn(){
+    
+    if(turnCounter === moveArray.length){
+        alert("oops!");
+        return;
+    }
+
+    let nextMove = document.querySelector(`[cellIndex="${moveArray[turnCounter]}"]`); //cell index of the next turn
+    placeHolder.unshift(moveArray[turnCounter]);
+    
+    if (turnCounter % 2 === 0)
+        nextMove.textContent = "X";
+    else
+        nextMove.textContent = "O";
+    turnCounter++;
 }
